6a019f16b39578683c4d917c002d43ef13fa8b24
Steven Everett <steven.everett@tiq.qld.gov.au>
2024-12-07 07:43:53 +1100

    adjusted wrapper paths

--- wrapper.py
+++ wrapper.py
@@ -1,6 +1,10 @@
 import subprocess
-import sys
+import os
 
 if __name__ == "__main__":
+     # Get the directory where the executable is located
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    # Construct the full path to main.py
+    main_path = os.path.join(base_dir, "main.py")
     # Call Streamlit to run the application
-    subprocess.run(["streamlit", "run", "main.py"])
+    subprocess.run(["streamlit", "run", main_path])
