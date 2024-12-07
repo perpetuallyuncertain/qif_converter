b560cf6a81e4f5fef4a6ceff1bb201587d5c75bb
Steven Everett <steven.everett@tiq.qld.gov.au>
2024-12-07 11:36:08 +1100

    Build Mac OS x86_64

--- wrapper.py
+++ wrapper.py
@@ -1,16 +1,25 @@
 import os
 import subprocess
 import sys
+import stat
 
 # Locate the PyInstaller temporary folder
 current_dir = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
 
-# Build the path to streamlit in the extracted folder
+# Path to the streamlit binary
 streamlit_path = os.path.join(current_dir, 'streamlit')
 
-# Ensure streamlit exists in the bundled files
+# Ensure streamlit exists
 if not os.path.exists(streamlit_path):
     print(f"Error: Streamlit not found at: {streamlit_path}")
+    sys.exit(1)
+
+# Set executable permissions for streamlit
+try:
+    st = os.stat(streamlit_path)
+    os.chmod(streamlit_path, st.st_mode | stat.S_IEXEC)
+except Exception as e:
+    print(f"Error setting executable permissions for Streamlit: {e}")
     sys.exit(1)
 
 # Path to main.py
